0d6dfc737874e29199f147491f22eefa270d5be5
Tiago <ztiagok@gmail.com>
2022-05-11 18:12:16 -0500

    c8 - Resolução do 'Product Details'

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,25 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se productDetails é uma função.
+      expect(typeof productDetails).toBe('function')
+
     // Teste se o retorno da função é um array.
+      expect(Array.isArray(productDetails())).toBeTruthy()
+
     // Teste se o array retornado pela função contém dois itens dentro.
+      expect(productDetails().length).toBe(2)
+
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+      expect(typeof productDetails()[0]).toBe('object')
+      expect(typeof productDetails()[1]).toBe('object')
+
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+      expect(productDetails()[0] !== productDetails()[1]).toBeTruthy();
+
     // Teste se os dois productIds terminam com 123.
+      expect(productDetails()[0].details.productId.endsWith('123') && (productDetails()[1].details.productId.endsWith('123'))).toBeTruthy();
   });
 });
