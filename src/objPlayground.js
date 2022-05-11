c492319e363036cf749b101c4f16be57cebecda4
Tiago <ztiagok@gmail.com>
2022-05-11 17:40:06 -0500

    c7 - Resolução do 'Object Playground'

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,21 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({
+  sum: Math.round(number1 + number2),
+  mult: Math.round(number1 * number2),
+  div: Math.floor(number1 / number2),
+  sub: Math.round(number1 - number2),
+  });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+  } if (type === 'values') {
+    return Object.values(object);
+  } if (type === 'entries') {
+    return Object.entries(object);
+  }
+};
 
 module.exports = { calculator, arrayGenerator };
