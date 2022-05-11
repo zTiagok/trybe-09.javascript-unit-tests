440636eec71d89fdcdaf6a22e46f12ee14774b0f
Tiago <ztiagok@gmail.com>
2022-05-11 17:27:20 -0500

    c6 - Resolução do 'Numbers'

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,17 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+      expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
+
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+      expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
+
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+      expect(numbers([1, 'a', 3])).toBeFalsy();
+
     // Escreva um teste em que a função recebe [' '] e retorna false
+      expect(numbers([' '])).toBeFalsy();
   });
 });
